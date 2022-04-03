4d9fa647fbff57a21398220f887270c3242ffcab
liuhao <liuhao@taou.com>
2022-04-04 00:13:09 +0800

    feat: child app

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, { useRef, createRef, useCallback, useEffect } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
-import Child from "./Child";
+import Home from "./routes/Home";
 // import { useImmer } from "./hooks/useImmer";
 // import produce from "immer";
 import { useImmer, useMountedState } from "@fcc/hooks";
@@ -9,30 +10,38 @@ import { atomWithImmer, useImmerAtom } from "jotai/immer";
 const manageAtomObj = atomWithImmer({ num: 1 });
 
 function App() {
-  console.log("父组件渲染了");
-  const [count, setCount] = useImmer({ num: 1 });
-  const isMoundFn = useMountedState();
-  console.log('isMoundFn', isMoundFn());
-  // const [count, setCount] = useImmerAtom(manageAtomObj);
-  const handleAppButtonClick = () => {
-    setCount((draft) => {
-      draft.num = draft.num + 1;
-    });
-  };
-  useEffect(() => {
-    console.log('isMoundFn', isMoundFn());
-  }, [])
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <button onClick={handleAppButtonClick}>change Count</button>
-        父组件:{count.num}
-        <p>-----------------------------------------------------</p>
-        <Child count={count} />
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
+
+  // console.log("父组件渲染了");
+  // const [count, setCount] = useImmer({ num: 1 });
+  // const isMoundFn = useMountedState();
+  // console.log("isMoundFn", isMoundFn());
+  // // const [count, setCount] = useImmerAtom(manageAtomObj);
+  // const handleAppButtonClick = () => {
+  //   setCount((draft) => {
+  //     draft.num = draft.num + 1;
+  //   });
+  // };
+  // useEffect(() => {
+  //   console.log("isMoundFn", isMoundFn());
+  // }, []);
+
+  // return (
+  //   <div className="App">
+  //     <header className="App-header">
+  //       <button onClick={handleAppButtonClick}>change Count</button>
+  //       父组件:{count.num}
+  //       <p>-----------------------------------------------------</p>
+  //       <Child count={count} />
+  //     </header>
+  //   </div>
+  // );
 }
 
 export default App;
---
+++ src/routes/Home.tsx
@@ -0,0 +1,34 @@
+import React, { useRef, createRef, useCallback, useEffect } from "react";
+import { useImmer, useMountedState } from "@fcc/hooks";
+import { atomWithImmer, useImmerAtom } from "jotai/immer";
+import Child from "../components/Child";
+const manageAtomObj = atomWithImmer({ num: 1 });
+
+function Home() {
+  console.log("父组件渲染了");
+  const [count, setCount] = useImmer({ num: 1 });
+  const isMoundFn = useMountedState();
+  console.log("isMoundFn", isMoundFn());
+  // const [count, setCount] = useImmerAtom(manageAtomObj);
+  const handleAppButtonClick = () => {
+    setCount((draft) => {
+      draft.num = draft.num + 1;
+    });
+  };
+  useEffect(() => {
+    console.log("isMoundFn", isMoundFn());
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <button onClick={handleAppButtonClick}>change Count</button>
+        父组件:{count.num}
+        <p>-----------------------------------------------------</p>
+        <Child count={count} />
+      </header>
+    </div>
+  );
+}
+
+export default Home;
