d3e87a0c12b87546be915ac8b8c1ad2b46fe850c
liuhao <liuhao@taou.com>
2022-04-04 21:05:04 +0800

    feat: test fetch cloudfire

--- src/components/Child.tsx
+++ src/components/Child.tsx
@@ -1,10 +1,15 @@
-import React from "react";
+import React, {useEffect} from "react";
 // import { useAtom } from "jotai";
 // import { numAtom } from "./store";
 // export default React.memo(function Child(props) {
 function Child(props: any) {
   console.log("子组件渲染了", props);
   // const [num, setNum] = useAtom(numAtom);
+  useEffect(() => {
+    fetch('http://127.0.0.1:8787/api')
+    .then(response => response.json())
+    .then(data => console.log(data));
+  }, [])
   return (
     <div>
       子组件: 
